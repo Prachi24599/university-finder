f64b62465f54dc14d8e1aeb975453d082d986c66
Prachi24599 <prachi24599@gmail.com>
2023-09-28 21:06:00 +0530

    api call using async-await

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
 import CountryDropdown from "./components/CountryDropdown";
 
 function App() {
+  const [selectedCountry, setSelectedCountry] = useState("Sweden");
+
+  const fetchUniversities = async (country) => {
+    try {
+      const response = await fetch(
+        `http://universities.hipolabs.com/search?country=${country}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching universities:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUniversities(selectedCountry);
+  }, []);
+
   return (
     <div className="App">
       <h1>University Finder</h1>
