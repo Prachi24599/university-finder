9d497e5c978af40ecf15af5467ce1bcdd3832b5f
Prachi24599 <prachi24599@gmail.com>
2023-09-29 11:32:30 +0530

    test case added

--- src/components/CountryDropdown.test.js
+++ src/components/CountryDropdown.test.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom"; // for `toHaveValue` matcher
+import CountryDropdown from "./CountryDropdown";
+import { UniversityProvider } from "../context/UniversityContext";
 
 // Mock the context values for testing
 const contextValues = {
-  selectedCountry: "Sweden",
-  setSelectedCountry: jest.fn(),
+  selectedCountry: "Sweden", // Initial selected country
+  setSelectedCountry: jest.fn(), // Mocked setSelectedCountry function
 };
+
+// Wrap the CountryDropdown component with the UniversityProvider context
+const renderWithProvider = (component) => {
+  return render(
+    <UniversityProvider value={contextValues}>{component}</UniversityProvider>
+  );
+};
+
+test("renders the country dropdown correctly", () => {
+  const countries = ["Sweden", "Norway", "India", "United States"];
+
+  renderWithProvider(<CountryDropdown countries={countries} />);
+
+  // Ensure the label and select element are rendered
+  expect(screen.getByText(/Select a Country/i)).toBeInTheDocument();
+  expect(screen.getByRole("combobox")).toBeInTheDocument();
+
+  // Check if options are rendered for each country
+  countries.forEach((country) => {
+    expect(screen.getByText(country)).toBeInTheDocument();
+  });
+});
+
+test("handles country selection", () => {
+  const countries = ["Sweden", "Norway", "India", "United States"];
+
+  renderWithProvider(<CountryDropdown countries={countries} />);
+
+  const selectElement = screen.getByRole("combobox");
+
+  // Simulate selecting a country from the dropdown
+  fireEvent.change(selectElement, { target: { value: "India" } });
+
+  // Check if setSelectedCountry was called with the selected value
+  expect(contextValues.setSelectedCountry).toHaveBeenCalledWith("India");
+  expect(selectElement).toHaveValue("India");
+});
